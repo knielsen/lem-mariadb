731b6077fd4a271d31528dc221f0ca2c46629e5c
Kristian Nielsen <knielsen@knielsen-hq.org>
2016-01-18 11:58:14 +0100

    Make it a bit simpler to change test.lua DB credentials.

--- test.lua
+++ test.lua
@@ -63,11 +63,12 @@ end
 
 local mariadb = require 'lem.mariadb'
 
-local db = assert(mariadb.connect(nil, 'user', 'pass', 'test', nil, '/var/run/mysqld/mysqld.sock'))
+local user,pass,sock = 'user', 'pass', '/var/run/mysqld/mysqld.sock'
+local db = assert(mariadb.connect(nil, user, pass, 'test', nil, sock))
 -- A prepared statement whose db object can be garbage-collected.
 local s_gc = assert(db:prepare("SELECT * FROM (SELECT 1 a, 'a' b UNION ALL SELECT 2 a, 'c' b UNION ALL SELECT 4 a, 'hulubulu' b) tmp ORDER BY a DESC"))
-db = assert(mariadb.connect('localhost', 'user', 'pass', 'test', nil, '/var/run/mysqld/mysqld.sock'))
-local db = assert(mariadb.connect('127.0.0.1', 'user', 'pass', 'test', 3306))
+db = assert(mariadb.connect('localhost', user, pass, 'test', nil, sock))
+local db = assert(mariadb.connect('127.0.0.1', user, pass, 'test', 3306))
 
 assert(db:exec(
 'DROP TABLE IF EXISTS mytable'))
